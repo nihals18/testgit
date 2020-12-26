4258cb9a4518b6d603ec97db4f3b14c61652f222
nihal <nihal.shikalgar@perennialsys.com>
2020-12-26 10:35:39 +0530

    SecondProgram.java added

---
+++ TestGit/src/main/java/com/git/test/SecondProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.git.test;
+
+/**
+ * @author perennial
+ *
+ */
+public class SecondProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
